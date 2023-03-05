3388d53b0d2489e4ef6a3dc30d2d411773d9ebb0
mateushlsilva <mateushls01@gmail.com>
2023-03-05 11:33:46 -0300

    adicionado loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut cont = 0;
+
+    let result = loop {
+        cont += 1;
+
+        if cont == 10 {
+            break cont * 2;
+        }
+    };
+
+    println!("O resultado é {result}")
 }
